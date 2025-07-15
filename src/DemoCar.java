8047b73768e0e3b4982da4d6b691efe56a1abb8e
Suman choudhary <sc108807@gmail.com>
2025-07-16 03:20:03 +0530

    updated

--- src/DemoCar.java
+++ src/DemoCar.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//car
+public class DemoCar {
+    String make;
+    String model;
+    int year;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    DemoCar(String make, String model, int year ){
+        this.make = make;
+        this.model = model;
+        this.year = year;
     }
+    void displayDetails(){
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("year: " + year);
+
+    }
+
+
+    public static void main(String[] args){
+        DemoCar myCar =  new DemoCar("Toyota", "Corolla", 2020);
+        myCar.displayDetails();
+    }
+
 }
